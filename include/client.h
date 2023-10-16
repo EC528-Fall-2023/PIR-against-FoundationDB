8d96ee241726001282d0c879eda62c4b68425073
ajamias <ajamias@bu.edu>
2023-10-16 18:26:56 -0400

    testing client implementation of Path ORAM

--- include/client.h
+++ include/client.h
@@ -1,28 +1,21 @@
 #ifndef PORAM_CLIENT_H
 #define PORAM_CLIENT_H
 
-#include <stdint.h>
+#include <string>
 
-/*
+class PathOramClient {
+public:
+	PathOramClient(const std::string &server_ip);
+	~PathOramClient();
 
-*/
-void put(uint8_t const *key_name, int key_name_length, uint8_t const *value, int value_length);
+	int put(const std::string &key_name, const std::string &value);
+	int get(const std::string &key_name, std::string &value);
+	int read_range(const std::string &begin_key_name, const std::string &end_key_name);
+	int clear_range(const std::string &begin_key_name, const std::string &end_key_name);
 
-/*
+private:
+	
+};
 
-*/
-uint8_t get(uint8_t const *key_name, int key_name_length);
-
-/*
-
-*/
-void read_range(uint8_t const *begin_key_name, int begin_key_name_length, 
-		uint8_t const *end_key_name, int end_key_name_length);
-
-/*
-
-*/
-void clear_range(uint8_t const *begin_key_name, int begin_key_name_length, 
-		 uint8_t const *eng_key_name, int end_key_name_length);
 
 #endif
