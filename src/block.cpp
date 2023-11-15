098a6642b8bfc785766e3fe43fbb67a22afaf84a
ajamias <ajamias@bu.edu>
2023-11-15 23:04:34 +0000

    update block class to C-like interface + encryption interface

--- src/block.cpp
+++ src/block.cpp
@@ -1,50 +1,96 @@
 #include "block.h"
 
-static std::random_device generator;
-static std::uniform_int_distribution<uint8_t> random_byte(0x00, 0xFF);
-
 Block::Block()
 {
-	block_id = 0;
-	for (uint8_t &byte: data) {
-		byte = random_byte(generator);
+	memset(&bytes, 0, sizeof(union bytes));
+	is_encrypted = false;
+}
+
+Block::Block(const Block &block)
+{
+	memcpy(&bytes, &block.bytes, sizeof(union bytes));
+	is_encrypted = block.is_encrypted;
+}
+
+Block::Block(uint16_t block_id, const uint8_t *data, uint32_t data_size)
+{
+	bytes.data_dec.block_id = block_id;
+	if (data_size < BYTES_PER_BLOCK) {
+		memcpy(bytes.data_dec.data, data, data_size);
+		memset(bytes.data_dec.data + data_size, 0, BYTES_PER_BLOCK - data_size);
+	} else {
+		memcpy(bytes.data_dec.data, data, BYTES_PER_BLOCK);
+	}
+	is_encrypted = false;
+}
+
+void Block::swap(Block &block)
+{
+	union bytes temp = {0};
+	memcpy(&temp, &bytes, sizeof(union bytes));
+	memcpy(&bytes, &block.bytes, sizeof(union bytes));
+	memcpy(&block.bytes, &temp, sizeof(union bytes));
+}
+
+inline uint16_t Block::get_block_id()
+{
+	return bytes.data_dec.block_id;
+}
+
+inline uint8_t *Block::get_decrypted_data()
+{
+	return bytes.data_dec.data;
+}
+
+inline uint8_t *Block::get_encrypted_data()
+{
+	return bytes.data_enc.data;
+}
+
+inline void Block::set_block_id(uint16_t block_id)
+{
+	bytes.data_dec.block_id = block_id;
+}
+
+void Block::set_decrypted_data(const uint8_t *data, uint32_t data_size)
+{
+	if (data_size < BYTES_PER_BLOCK) {
+		memcpy(bytes.data_dec.data, data, data_size);
+		memset(bytes.data_dec.data + data_size, 0, BYTES_PER_BLOCK - data_size);
+	} else {
+		memcpy(bytes.data_dec.data, data, BYTES_PER_BLOCK);
 	}
 }
 
-Block::Block(const Block &block) {
-	this->block_id = block.block_id;
-	this->data = block.data;
-}
-
-Block::Block(uint16_t block_id, const std::array<uint8_t, BYTES_PER_BLOCK>& data)
+void Block::set_encrypted_data(const uint8_t *data, uint32_t data_size)
 {
-	this->block_id = block_id;
-	this->data = data;
-}
-
-uint16_t Block::get_block_id()
-{
-	return block_id;
-}
-
-std::array<uint8_t, BYTES_PER_BLOCK> &Block::get_data()
-{
-	return data;
-}
-
-void Block::set_block_id(uint16_t block_id)
-{
-	this->block_id = block_id;
-}
-
-void Block::set_data(const std::array<uint8_t, BYTES_PER_BLOCK>& data)
-{
-	this->data = data;
-}
-
-void Block::set_random_data()
-{
-	for (uint8_t &byte: data) {
-		byte = random_byte(generator);
+	if (data_size < BYTES_PER_BLOCK + BLOCK_ID_SIZE) {
+		memcpy(bytes.data_enc.data, data, data_size);
+		memset(bytes.data_enc.data + data_size, 0, BYTES_PER_BLOCK + BLOCK_ID_SIZE - data_size);
+	} else {
+		memcpy(bytes.data_enc.data, data, BYTES_PER_BLOCK + BLOCK_ID_SIZE);
 	}
 }
+
+void Block::set_decrypted_random_data()
+{
+	std::random_device generator;
+	std::uniform_int_distribution<uint8_t> random_byte(0x00, 0xFF);
+
+	for (uint32_t i = 0; i < BYTES_PER_BLOCK; ++i) {
+		bytes.data_dec.data[i] = random_byte(generator);
+	}
+}
+
+int Block::encrypt(uint8_t *key, uint8_t *iv)
+{
+	// put encryption here
+	is_encrypted = true;
+}
+
+int Block::decrypt(uint8_t *key, uint8_t *iv)
+{
+	// put decryption here
+	is_encrypted = false;
+}
+
