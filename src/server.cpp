bc87924f14213973b93b5762b8b570a213597568
Ruben304 <ruben.carbajal6502@gmail.com>
2023-12-02 12:15:31 -0500

    fixing formating

--- src/server.cpp
+++ src/server.cpp
@@ -460,13 +460,13 @@ inline int get_branch_from_fdb(std::vector<Block> &branch, std::vector<blkid_t> 
 				}
 #ifdef DEBUG
 
-            file << "BlkID: ,Data: ";
+            file << " Getting BlkID: ";
             // Printing key as a hex string in the first column
             for (int i = sizeof(blkid_t); i != 0; --i) {
                 file << std::hex << std::setfill('0') << std::setw(2)
                      << ((branch_indexes[current_bucket] >> 8 * (sizeof(blkid_t) - i)) & 0xff);
             }
-            file << ","; // Delimiter
+            file << ", Getting Data: "; // Delimiter
             // Write value as a hex string in the second column
             for (int i = 0; i < out_value_length; ++i) {
                 char val = static_cast<char>(out_value[i]);
@@ -564,12 +564,12 @@ inline int send_branch_to_fdb(std::vector<Block> &branch, std::vector<blkid_t> &
 #ifdef DEBUG
         // CSV writing part
         // Printing key as a hex string in the first column
-        file << "BlkID: ,Data: "; // Headers for the columns
+        file << "Sending BlkID: "; // Headers for the columns
         for (int i = sizeof(blkid_t); i != 0; --i) {
             file << std::hex << std::setfill('0') << std::setw(2)
                  << ((branch_indexes[current_bucket] >> 8 * (sizeof(blkid_t) - i)) & 0xff);
         }
-        file << ","; // Delimiter
+        file << ",Sending Data: "; // Delimiter
 
         // Write value as a hex string in the second column
         for (int i = 0; i < BLOCK_SIZE * BLOCKS_PER_BUCKET; ++i) {
